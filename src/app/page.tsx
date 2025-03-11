ef2c41f7ead6d75a02e2153aa5bbc28d8a90e9c3
Kalan-47 <krishnaaswin490@gmail.com>
2025-03-11 10:43:03 +0530

    header modified+detailed menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import Image from "next/image";
 import NewsCalendarSection from "@/components/NewsCalendarSection";
 import GallerySection from "@/components/GallerySection";
+import { DetailedMenu } from "@/components/DetailedMenu";
 
 export default function Home() {
     return (
